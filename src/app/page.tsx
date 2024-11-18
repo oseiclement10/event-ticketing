589e9c866f2e63dbadd337cf900d8fc7ea311a0e
Clement Osei Asante <clementoseiasante.nss@gmail.com>
2024-11-18 23:09:09 +0000

    hero exploration 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 import Header from "./components/Header";
 import partyImg from "../../public/assets/party.jpg";
+import weekendParty from "../../public/assets/weekendparty.jpg";
 import heroLady from "../../public/assets/heroLady.png";
 
 export default function Home() {
@@ -23,17 +24,28 @@ export default function Home() {
           </div>
         </div>
         <div className="relative ">
-          <div className="grid relative z-10 grid-cols-2 border-white border  bg-gray-100/30   backdrop-blur-3xl  px-8 gap-8 py-10 ">
-            <EventHeroCard />
-            <EventHeroCard />
+          <div className="grid relative z-10 grid-cols-2 border-white border  bg-gray-100/10   backdrop-blur-3xl  px-8 gap-8 py-10 ">
             <div className="col-span-2 flex items-center justify-center">
-              <Image src={heroLady} alt="hero lady" className="w-[200px] " />
+              <div className="relative border rounded-3xl overflow-hidden bg-gray-100/40 backdrop-blur-sm border-white max-w-[300px]">
+                <Image src={heroLady} alt="hero lady" className="-z-10 relative  w-[160px] mx-auto" />
+                <div className="bg-white px-4 py-2 -mt-9 z-10">
+                  <h3 className="font-semibold text-slate-800 mb-1">Hero Header</h3>
+                  <p className="text-sm text-slate-800 mb-3">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum voluptate fugiat rerum.</p>
+                  <div className="flex items-center space-x-1">
+                    <span className=" flex h-2 w-2 rounded-full bg-emerald-600"></span>
+                    <span className=" flex h-2 w-2 rounded-full bg-emerald-600"></span>
+                    <span className=" flex h-2 w-2 rounded-full bg-emerald-600"></span>
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
+            <EventHeroCard img={partyImg} />
+            <EventHeroCard img={weekendParty} />
 
-          <div className="absolute zigzag-reverse bottom-0 right-0 w-[220px] h-[180px] rounded-3xl  bg-emerald-400" />
-          <div className="absolute zigzag  inset-0 w-[220px] h-[180px] rounded-3xl  bg-rose-500" />
-          <div className="absolute astronaut  right-1/2 w-[220px] h-[180px] rounded-3xl  bg-violet-500" />
+          </div>
+          <div className="absolute zigzag  inset-0 w-[220px] h-[180px] rounded-3xl mix-blend-multiply  bg-violet-500" />
+          <div className="absolute zigzag-reverse bottom-0 right-0 w-[220px] h-[180px] rounded-3xl mix-blend-multiply  bg-amber-400" />
+          <div className="absolute astronaut  right-1/2 w-[220px] h-[180px] rounded-3xl mix-blend-multiply  bg-rose-500" />
 
 
         </div>
@@ -43,10 +55,10 @@ export default function Home() {
   );
 }
 
-const EventHeroCard = () => {
+const EventHeroCard = ({img}: {img: StaticImageData}) => {
   return <div className="rounded-3xl border border-white shadow-sm w-[280px] bg-white/60 backdrop-blur-md h-fit  overflow-hidden ">
     <div className="row-span-3">
-      <Image src={partyImg} alt="Party Event" className="w-full h-[140px] object-cover" />
+      <Image src={img} alt="Party Event" className="w-full h-[140px] object-cover" />
     </div>
     <div className="px-4 pt-3 pb-6  ">
 
